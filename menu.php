38c0be564769bc8332634584ccf71955531fca17
nodarzakirovich <noligarx@gmail.com>
2018-11-03 19:16:36 +0500

    Mysite

---
+++ menu.php
@@ -0,0 +1,21 @@
+<?php 
+$menu = array(
+      array( 'About','about.php', 'fa fa-book'),
+      array('Services','services.php','fa fa-cogs' ),
+      array('Contact','contact.php','fa fa-address-card-o'),
+      array('Portfolio','portfolio.php','fa fa-flag-checkered' ),
+      array('Blog','blog.php','fa fa-group'),
+      array('News','news.php','fa fa-angle-double-down'),
+      array( 'Other Page','other_pages.php','fa fa-angle-double-down'),
+      array('Something', 'some.php', 'fa fa-cloud')	
+ );
+$slider = array(
+       array(0, 'http://placehold.it/1900x1080','active','First Slide', 'This is a description for the first slide'),
+       array(1, 'http://placehold.it/1900x1080','','Second Slide', 'This is a description for the second slide'),
+       array(2, 'http://placehold.it/1900x1080','','Third Slide', 'This is a description for the third slide'),
+       
+      
+  
+);
+
+?>
